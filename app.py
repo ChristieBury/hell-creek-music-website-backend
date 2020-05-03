79903041703284859cbde7de375343e1a55e71b3
makoshika <buryc405@bucs.dawson.edu>
2020-05-03 13:49:52 -0600

    modified appy

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_heroku import Heroku
 
 app = Flask(__name__)
 CORS(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://osxnickyzjyjpi:2a8b9ac5075c647d0ddfa9cd165610b108d6a45e66b38cde28e14512d76059ff@ec2-52-86-73-86.compute-1.amazonaws.com:5432/ddljo5kimoedja"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://ktwphuotkumojt:9440478b8e98af2aceef4aa90a2d37499399a3b10f039f9b10c310413f07615d@ec2-54-159-112-44.compute-1.amazonaws.com:5432/d9e7mvfl4j8920"
 heroku = Heroku(app)
 db = SQLAlchemy(app)
 
@@ -14,66 +14,66 @@ class Comic(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(200))
     author = db.Column(db.String)
-    image = db.Column(db.String)
+    # image = db.Column(db.String)
 
-    def __init__(self, title, author, image):
+    def __init__(self, title, author): #image):
         self.title = title
         self.author = author
-        self.image = image
+        # self.image = image
 
     def __repr__(self):
-        return f"Title{self.title}"         
+        # return f"Title{self.title}"   
+        return '<Title%r>' % self.title      
 
 @app.route('/')
 def home():
     return "<h1>Comics!</h1>"
 
-# @app.route('/comic-books/input', methods=['POST'])
-# def comics_input():
-#     if request.content_type == 'application/json':
-#         post_data = request.get_json()
-#         title = post_data.get('title')
-#         author = post_data.get('author')
-#         image = post_data.get('image')
-#         reg = Comic(title, author, image)
-#         db.session.add(reg)
-#         db.session.commit()
-#         return jsonify("Data Posted")
-#     return jsonify('Did not post') 
+@app.route('/comic-books/input', methods=['POST'])
+def comics_input():
+    if request.content_type == 'application/json':
+        post_data = request.get_json()
+        title = post_data.get('title')
+        author = post_data.get('author')
+        # image = post_data.get('image')
+        reg = Comic(title, author)
+        db.session.add(reg)
+        db.session.commit()
+        return jsonify("Data Posted")
+    return jsonify('Did not post') 
 
-# @app.route('/comic-books', methods = ['GET'])
-# def return_comics():
-#     all_comics = db.session.query(Comic.id, Comic.title, Comic.author, Comic.image).all()
-#     return jsonify(all_comics)
+@app.route('/comic-books', methods = ['GET'])
+def return_comics():
+    all_comics = db.session.query(Comic.id, Comic.title, Comic.author).all()
+    return jsonify(all_comics)
 
-# @app.route('/comic-books/<id>', methods = ['GET'])
-# def return_single_comic(id):
-#     one_comic = db.session.query(Comic.id, Comic.title, Comic.author, Comic.image).filter(Comic.id == id).first()
-#     return jsonify(one_comic)
+@app.route('/comic-book/<id>', methods = ['GET'])
+def return_single_comic(id):
+    one_comic = db.session.query(Comic.id, Comic.title, Comic.author).filter(Comic.id == id).first()
+    return jsonify(one_comic)
 
-# @app.route('/delete/<id>', methods = ['DELETE'])
-# def comic_delete(id):
-#     if request.content_type == 'application/json':
-#         record = db.session.query(Comic).get(id)
-#         db.session.delete(record)
-#         db.session.commit()
-#         return jsonify('completed Comic deletion')
-#     return jsonify('deletion failed')
+@app.route('/delete/<id>', methods = ['DELETE'])
+def comic_delete(id):
+    record = db.session.query(Comic).get(id)
+    db.session.delete(record)
+    db.session.commit()
+    return jsonify('completed Comic deletion')
+   
 
-# @app.route('/update_comic/<id>', methods = ['PUT'])
-# def comic_update(id):
-#     if request.content_type == 'application/json':
-#         put_data = request.get_json()
-#         title = put_data.get('title')
-#         author = put_data.get('author')
-#         image = put_data.get('image')
-#         record = db.session.query(Comic).get(id)
-#         record.title = title
-#         record.author = author
-#         record.image = image
-#         db.session.commit()
-#         return jsonify('Completed Comic Page Update')
-#     return jsonify('Failed Update')
+@app.route('/update_comic/<id>', methods = ['PUT'])
+def comic_update(id):
+    if request.content_type == 'application/json':
+        put_data = request.get_json()
+        title = put_data.get('title')
+        author = put_data.get('author')
+        # image = put_data.get('image')
+        record = db.session.query(Comic).get(id)
+        record.title = title
+        record.author = author
+        # record.image = image
+        db.session.commit()
+        return jsonify('Completed Comic Page Update')
+    return jsonify('Failed Update')
     
 if __name__ == '__main__':
     app.run(debug=True)
